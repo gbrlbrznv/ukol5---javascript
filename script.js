239f0838a936b9d0938c5b50900334c619ff98f6
Gabriela Březinová <gabri.brezinova@gmail.com>
2024-03-17 17:56:13 +0100

    #message

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 /* Vytvořte si repozitář ze šablony cviceni-cenik se stránkou, která nabízí předplatné za 
 nějaké služby. Může jít například o internetovou televizi, pravidelné dovážky jídla nebo 
 třeba webový hosting.
+
 1. Přidejte to stránky soubor s JavaScriptem.
 2. Napište funkci selectPlan s jedním parametrem planNumber. Tento parametr bude 
 představovat číslo plánu. Funkce podle čísla plánu vybere ze stránky správný DOM 
@@ -12,16 +13,19 @@ tak, že vybere plán zadaný v parametru a u ostatních plánů výběr zruší
 stačí z příslušného prvku odebrat třídu plan--selected.
 5. Opět vyzkoušejte funkci ve stránce. Neuvidíte výběr jednotlivých plánů 
 (JavaScript je vybere velmi rychle), ale na stránce by měl zůstat poslední vybraný plán. */
+
 const selectPlan = (planNumber) => {
     const allElements = document.querySelectorAll('.plan')
     allElements.forEach((element) => {
         element.classList.remove('plan--selected')
     })
+
     const selectedPlan = document.querySelector(`#plan${planNumber}`)
     if (selectedPlan) {
         selectedPlan.classList.add('plan--selected')
     }
 }
+
 selectPlan(1)
 selectPlan(2)
 selectPlan(3)
